f965a690d5281298faefaabfc5772df7337b3de9
Antonio Reid <read.musik@gmail.com>
2018-04-11 16:31:01 +0000

    automatically backed up by learn

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,24 @@
+module Slugifiable
+
+  module InstanceMethods
+
+    def slug
+      self.name.downcase.strip.gsub(' ', '-')
+    end
+
+  end
+
+  module ClassMethods
+
+    def find_by_slug(slug)
+      slug_arr = slug.gsub('-', ' ').split(' ')
+      slug_arr.collect! do |w|
+        w.capitalize
+      end
+      name = slug_arr.join(' ')
+      result = self.find_by_name(name)
+    end
+
+  end
+
+end
