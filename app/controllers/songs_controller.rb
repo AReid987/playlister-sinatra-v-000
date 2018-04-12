799a472304291d0fc5601c66dfa940bf7b68014d
Antonio Reid <read.musik@gmail.com>
2018-04-12 00:55:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,7 @@ class SongsController < ApplicationController
 
   post '/songs' do
     if !Artist.all.include?(params[:song][:artist])
+      @artist = Artist.create(name: params[:song][:artist])
     end 
     binding.pry
 
