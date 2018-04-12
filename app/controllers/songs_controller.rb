ae8351fcd1090dca2ad47e9350685ec08d8df403
Antonio Reid <read.musik@gmail.com>
2018-04-12 02:17:34 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
+    binding.pry
     Artist.all.each do |artist|
       if artist.name == params[:song][:artist]
         @artist = artist
